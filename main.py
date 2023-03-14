35d499d299ea75dd6e56cd85e08d04def4677d79
likith-gullapudi <gullapudilikith@gmail.com>
2023-03-14 14:59:32 +0530

    nrg

---
+++ main.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+# Put this somewhere safe!
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+
+
+f.decrypt(token)
+b'A really secret message. Not for prying eyes.'
+
+
